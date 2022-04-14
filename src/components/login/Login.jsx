7232c2aed9459462d898f7b7622e5a03e88a37a9
Emmanuel Granara <emmanuelgranara@hotmail.com>
2022-04-14 16:48:05 -0300

    login styles

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import styled from 'styled-components';
+import styled from "styled-components";
 import { ToastContainer } from "react-toastify";
 import { ErrorMessage, Field, Form, Formik } from "formik";
 import Loading from "../Loading/Loading";
@@ -10,14 +10,49 @@ import { login } from "../../redux/apiCalls";
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
-  background-Color: gray;
+  background-color: gray;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
+const Wrapper = styled.div`
+  width: 40%;
+  padding: 5% 7%;
+  border-radius: 15px;
+  background-color: white;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 10px 0px;
+  padding: 10px;
+  min-width: -webkit-fill-available;
+  max-width: -webkit-fill-available;
+`;
+
+const Button = styled.button`
+  width: 100%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+  margin: 10px 0px;
+  &:disabled {
+    color: grey;
+    cursor: not-allowed;
+  }
+`;
+
 const Login = () => {
-  const history = useHistory(); 
+  const history = useHistory();
   const dispatch = useDispatch();
   const { isFetching } = useSelector((state) => state.user);
 
@@ -37,93 +72,62 @@ const Login = () => {
     }
   }, [isFetching, history]);
 
-
   return (
     <Container>
-      
-      <div className="row">
-        <div className="col-lg-6 offset-lg-3 col-md-8 offset-md-2 col-sm-10 offset-sm-1">
-          <h1 className="text-center mb-5">Enter the App</h1>
+      <Wrapper>
+        <Title>Enter the App</Title>
+        <Formik
+          initialValues={initialValues}
+          onSubmit={onSubmit}
+          validate={(values) => {
+            const errors = {};
+            if (!values.email) {
+              errors.email = "Email is required";
+            } else if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+            ) {
+              errors.email = "Invalid email address";
+            }
+            if (!values.password) {
+              errors.password = "Password is required";
+            } else if (
+              values.password.length < 5 ||
+              values.password.length > 5
+            ) {
+              errors.password = "password must be 5 characters";
+            }
+            return errors;
+          }}
+        >
+          {(formik) => {
+            return (
+              <Form>
+                <Input type="email" name="email" placeholder="Enter an email" />
+                <ErrorMessage name="email" component="p" />
+                <Input
+                  type="password"
+                  name="password"
+                  placeholder="Enter a password"
+                />
+                <ErrorMessage name="password" component="p" />
+                <Button type="submit" disabled={!formik.isValid}>
+                  LOGIN
+                </Button>
+              </Form>
+            );
+          }}
+        </Formik>
 
-          <div className="card border-2 mt-2">
-            <Formik
-              initialValues={initialValues}
-              onSubmit={onSubmit}
-              validate={(values) => {
-                const errors = {};
-                if (!values.email) {
-                  errors.email = "Email is required";
-                } else if (
-                  !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-                ) {
-                  errors.email = "Invalid email address";
-                }
-                if (!values.password) {
-                  errors.password = "Password is required";
-                } else if (
-                  values.password.length < 5 ||
-                  values.password.length > 5
-                ) {
-                  errors.password = "password must be 5 characters";
-                }
-                return errors;
-              }}
-            >
-              {(formik) => {
-                return (
-                  <Form className="p-4">
-                    <div className="mb-3">
-                      <label className="form-label">Email address</label>
-                      <Field
-                        className="form-control mb-2"
-                        type="email"
-                        name="email"
-                        placeholder="Enter an email"
-                      />
-                      <ErrorMessage
-                        className="mb-2 p-2 text-center fw-bold bg-info"
-                        name="email"
-                        component="p"
-                      />
-                    </div>
-                    <div className="mb-3">
-                      <label className="form-label">Password</label>
-                      <Field
-                        className="form-control mb-2"
-                        type="password"
-                        name="password"
-                        placeholder="Enter a password"
-                      />
-                      <ErrorMessage
-                        className="mb-2 p-2 text-center fw-bold bg-info"
-                        name="password"
-                        component="div"
-                      />
-                    </div>
-                    <button
-                      type="submit"
-                      className="btn"
-                      disabled={!formik.isValid}
-                      style={{ background: "cadetblue" }}
-                    >
-                      Log in
-                    </button>
-                  </Form>
-                );
-              }}
-            </Formik>
-          </div>
-          <div className="row">
-            {isFetching ? (
-              <div className="d-flex justify-content-center mt-5">
-                <Loading />
-              </div>
-            ) : null}
-          </div>
+        <div className="row">
+          {isFetching ? (
+            <div className="d-flex justify-content-center mt-5">
+              <Loading />
+            </div>
+          ) : null}
         </div>
-      </div>
-      <ToastContainer />
 
+        <ToastContainer />
+      </Wrapper>
     </Container>
   );
 };
