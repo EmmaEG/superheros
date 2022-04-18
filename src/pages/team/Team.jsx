dc9cee4e2811ce2a1cd77b85b85cf53bbb32c032
Emmanuel Granara <emanuelgranara@hotmail.com>
2022-04-18 20:03:16 -0300

    working on team page styles

--- src/pages/team/Team.jsx
+++ src/pages/team/Team.jsx
@@ -1,9 +1,58 @@
 import React from "react";
-import "./team.css";
-
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import styled from "styled-components";
 import { deleteHero } from "../../redux/teamSlice";
+
+const Container = styled.div`
+  display: flex;
+  width: 100vw;
+  height: calc(100vh - 60px);
+`;
+
+const Wrapper = styled.div`
+  padding: 20px 150px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  background-color: #cbcfd1;
+`;
+
+const CardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 10px;
+  min-width: 280px;
+  max-width: 280px;
+  height: 350px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #ffffff;
+  position: relative;
+  border-radius: 10px;
+`;
+
+const CardImg = styled.img`
+  height: 89%;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+`;
+
+const CardInfo = styled.div`
+  width: 100%;
+  padding: 10px 10px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const EmptyWrapper = styled.div`
+  margin: auto;
+`;
+
+const P = styled.p`
+  color: #ff0000;
+`;
 
 const Team = () => {
   const team = useSelector((state) => state.team);
@@ -15,41 +64,37 @@ const Team = () => {
   };
 
   return (
-    <div>
-    {team.heroes.length === 0 ? (
-        <div className="d-flex justify-content-center mt-5">
-                    <p className="message mt-5">For now... Your team is empty</p>
-        </div>
+    <Container>
+      {team.heroes.length === 0 ? (
+        <EmptyWrapper>
+          <P>For now... Your team is empty</P>
+        </EmptyWrapper>
       ) : (
-      <div className="mt-4">
-        <div className="row">
+        <Wrapper>
           {team.heroes.map((hero) => (
-            <div key={hero.id} className="col-12 col-md-6 col-lg-4 mb-4">
-              <div className="card" style={{ width: "18rem" }}>
-                <img
-                  style={{ height: "50vh" }}
-                  src={hero.image.url}
-                  className="card-img-top"
-                  alt={hero.name}
-                />
-                <div className="card-body d-flex justify-content-between">
-                  <h5 className="card-title">{hero.name}</h5>
-                  <button
-                    className="btn btn-danger"
-                    onClick={() => handleDeleteHero(hero)}
-                  >
-                    -
-                  </button>
-                  <Link to={`/details/${hero.id}`}>
-                    <button className="btn btn-info">+</button>
-                  </Link>
-                </div>
-              </div>
-            </div>
+            <CardContainer key={hero.id}>
+              <CardImg
+                src={hero.image.url}
+                className="card-img-top"
+                alt={hero.name}
+              />
+              <CardInfo>
+                <h5 className="card-title">{hero.name}</h5>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => handleDeleteHero(hero)}
+                >
+                  -
+                </button>
+                <Link to={`/details/${hero.id}`}>
+                  <button className="btn btn-info">+</button>
+                </Link>
+              </CardInfo>
+            </CardContainer>
           ))}
-        </div>
-      </div>)}
-      </div>
+        </Wrapper>
+      )}
+    </Container>
   );
 };
 
